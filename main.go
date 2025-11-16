4261282dd20fe6a61aa7d6527aaf9f636acdf43b
Vincent Handoko <vincenthandoko288@gmail.com>
2025-11-16 17:44:39 +0700

    validated input and output

--- main.go
+++ main.go
@@ -47,7 +47,10 @@ func main() {
 		os.Exit(0)
 	}
 
-	fmt.Println(mapping)
+	if err := validatetype(mapping); err != nil {
+		fmt.Printf("gagal memvalidasi tipe data: %s \n", err)
+		os.Exit(0)
+	}
 }
 
 func printUsage() {
@@ -118,5 +121,20 @@ func readInput(path string, mapping *map[string]string) error {
 		return err
 	}
 
+	return nil
+}
+
+func validatetype(mapping map[string]string) error {
+	supported := map[string]bool{
+		"name":    true,
+		"address": true,
+		"date":    true,
+		"phone":   true,
+	}
+	for _, value := range mapping {
+		if !supported[value] {
+			return errors.New("tipe data tidak didukung")
+		}
+	}
 	return nil
 }
